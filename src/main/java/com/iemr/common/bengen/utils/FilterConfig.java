92084de17e232eb55656146b2204b53bc4f1996b
KOPPIREDDY DURGA PRASAD <144464542+DurgaPrasad-54@users.noreply.github.com>
2026-02-11 19:44:21 +0530

    chore(swagger): automate swagger sync to amrit-docs (#59)
    * docs(readme): add documentation badge
    
    * chore(swagger): automate swagger sync to amrit-docs
    
    * chore(swagger): update swagger workflow
    
    * fix(swagger): fix github workflow running issue

--- src/main/java/com/iemr/common/bengen/utils/FilterConfig.java
+++ src/main/java/com/iemr/common/bengen/utils/FilterConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!swagger")
 public class FilterConfig {
 
 	@Value("${cors.allowed-origins}")
